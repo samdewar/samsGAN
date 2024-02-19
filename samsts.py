28ee6405cd5aef173dcb29a4e9c92530550c6995
Sam <samdewar@protonmail.com>
2024-02-19 13:50:31 +0000

    modular discriminator & generator

--- samsts.py
+++ samsts.py
@@ -12,175 +12,58 @@ from IPython import display
 
 
 dataset_path ="C:\\Users\\Sam\\PycharmProjects\\samsgan\\training_data\\"
-batch_size=3
-image_size=(571,571)
+BATCH_SIZE=3
+IMAGE_SIZE=(571,571)
 
 train_dataset=tf.keras.preprocessing.image_dataset_from_directory(
     dataset_path,
-    batch_size=batch_size,
-    image_size=image_size
+    batch_size=BATCH_SIZE,
+    image_size=IMAGE_SIZE
 )
 
-# train_dataset=train_dataset.map(lambda x,y:(tf.cast(x,tf.float32)/127.5-1.0,y))
-# train_dataset=train_dataset.c
-# Create Generator
-def make_generator_model():
-    model = tf.keras.Sequential()
-    model.add(layers.Dense(7*7*256, use_bias=False, input_shape=(100,)))
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
 
-    model.add(layers.Reshape((7, 7, 256)))
-    assert model.output_shape == (None, 7, 7, 256)  # Note: None is the batch size
+train_dataset=train_dataset.map(lambda x,y:(tf.cast(x,tf.float32)/127.5-1.0,y))
+train_dataset=train_dataset.cache().prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
 
-    model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
-    assert model.output_shape == (None, 7, 7, 128)
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
+noise_dim=100
+num_channels=3
+#
+def make_generator(height=7, width=7,inputChannels=256):
+    generator=tf.keras.Sequential()
+    generator.add(layers.Dense(height*width*inputChannels,use_bias=False,input_shape=(100,)))
+    generator.add(layers.BatchNormalization())
+    generator.add(layers.LeakyReLU())
 
-    model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
-    assert model.output_shape == (None, 14, 14, 64)
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
+    generator.add(layers.Reshape(height,width,inputChannels))
+    assert generator.output_shape == (None, height, width, inputChannels)
+    generator.add(layers.Conv2DTranspose(128,(5,5),strides=(1,1),padding='same', use_bias=False))
+    # assert generator.output_shape == (none,2)
+    generator.add(layers.BatchNormalization())
+    generator.add(layers.LeakyReLU)
 
-    model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
-    assert model.output_shape == (None, 28, 28, 1)
+    generator.add(layers.Conv2DTranspose(64,(5,5), strides=(2,2),padding='same',use_bias=False))
+    # assert generator
+    generator.add(layers.BatchNormalization())
+    generator.add(layers.LeakyReLU)
 
-    return model
-generator = make_generator_model()
+    generator.add(layers.Conv2DTranspose(1,(5,5),strides=(2,2),padding='same',use_bias=False))
+    # assert
 
-noise = tf.random.normal([1, 100])
-generated_image = generator(noise, training=False)
-
-plt.imshow(generated_image[0, :, :, 0], cmap='gray')
-
-# Create Discriminator
-def make_discriminator_model():
-    model = tf.keras.Sequential()
-    model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
-                                     input_shape=[28, 28, 1]))
-    model.add(layers.LeakyReLU())
-    model.add(layers.Dropout(0.3))
-
-    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
-    model.add(layers.LeakyReLU())
-    model.add(layers.Dropout(0.3))
-
-    model.add(layers.Flatten())
-    model.add(layers.Dense(1))
-
-    return model
-discriminator = make_discriminator_model()
-decision = discriminator(generated_image)
-print (decision)
-
-# Loss Functions
-# This method returns a helper function to compute cross entropy loss
-cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
-def discriminator_loss(real_output, fake_output):
-    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
-    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
-    total_loss = real_loss + fake_loss
-    return total_loss
-def generator_loss(fake_output):
-    return cross_entropy(tf.ones_like(fake_output), fake_output)
-generator_optimizer = tf.keras.optimizers.Adam(1e-4)
-discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
-# Checkpoints
-checkpoint_dir = './training_checkpoints'
-checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
-checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
-                                 discriminator_optimizer=discriminator_optimizer,
-                                 generator=generator,
-                                 discriminator=discriminator)
-
-# Loop
-EPOCHS = 50
-noise_dim = 100
-num_examples_to_generate = 16
-
-# You will reuse this seed overtime (so it's easier)
-# to visualize progress in the animated GIF)
-seed = tf.random.normal([num_examples_to_generate, noise_dim])
-# Notice the use of `tf.function`
-# This annotation causes the function to be "compiled".
-@tf.function
-def train_step(images):
-    noise = tf.random.normal([BATCH_SIZE, noise_dim])
-
-    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
-      generated_images = generator(noise, training=True)
-
-      real_output = discriminator(images, training=True)
-      fake_output = discriminator(generated_images, training=True)
-
-      gen_loss = generator_loss(fake_output)
-      disc_loss = discriminator_loss(real_output, fake_output)
-
-    gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
-    gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
-
-    generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
-    discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
-
-def train(dataset, epochs):
-  for epoch in range(epochs):
-    start = time.time()
-
-    for image_batch in dataset:
-      train_step(image_batch)
-
-    # Produce images for the GIF as you go
-    display.clear_output(wait=True)
-    generate_and_save_images(generator,
-                             epoch + 1,
-                             seed)
-
-    # Save the model every 15 epochs
-    if (epoch + 1) % 15 == 0:
-      checkpoint.save(file_prefix = checkpoint_prefix)
-
-    print ('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
-
-  # Generate after the final epoch
-  display.clear_output(wait=True)
-  generate_and_save_images(generator,
-                           epochs,
-                           seed)
-
-def generate_and_save_images(model, epoch, test_input):
-  # Notice `training` is set to False.
-  # This is so all layers run in inference mode (batchnorm).
-  predictions = model(test_input, training=False)
-
-  fig = plt.figure(figsize=(4, 4))
-
-  for i in range(predictions.shape[0]):
-      plt.subplot(4, 4, i+1)
-      plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
-      plt.axis('off')
-
-  plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
-  # plt.show()
-
-train(train_dataset, EPOCHS)
-checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
-
-# Display a single image using the epoch number
-def display_image(epoch_no):
-  return PIL.Image.open('image_at_epoch_{:04d}.png'.format(epoch_no))
-display_image(EPOCHS)
-
-anim_file = 'dcgan.gif'
-
-with imageio.get_writer(anim_file, mode='I') as writer:
-  filenames = glob.glob('image*.png')
-  filenames = sorted(filenames)
-  for filename in filenames:
-    image = imageio.imread(filename)
-    writer.append_data(image)
-  image = imageio.imread(filename)
-  writer.append_data(image)
+    return generator
 
 
+def make_discriminator(droupoutRate=0.3):
+    discriminator=tf.keras.Sequential()
+    discriminator.add(layers.Conv2D(64,(5,5),strides=(2,2),padding='same',input_shape=[IMAGE_SIZE,1]))
 
+    discriminator.add(layers.LeakyReLU())
+    discriminator.add(layers.Dropout(droupoutRate))
+
+    discriminator.add(layers.Conv2D(128,(5,5),strides=(2,2),padding='same'))
+    discriminator.add(layers.LeakyReLU())
+    discriminator.add(layers.Dropout(droupoutRate))
+
+    discriminator.add(layers.Flatten())
+    discriminator.add(layers.Dense(1))
+
+    return discriminator
